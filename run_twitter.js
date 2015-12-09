7855c7d46645d8d291786a1b162ce8189a7f1ae3
neatsun.ziv <neatsunz@gmail.com>
2015-12-09 23:19:55 +0200

    added image support

--- run_twitter.js
+++ run_twitter.js
@@ -1,5 +1,5 @@
 var nodemailer = require('nodemailer');
-
+ 
 var Twitter = require('twitter');
 
 console.log('consumer_key= '        + process.env.consumer_key);
@@ -15,31 +15,67 @@ console.log('send_digest_to_email= '+ process.env.send_digest_to_email);
 
 function main ()
 {
-	 
-	var client = new Twitter({
-	  consumer_key         : process.env.consumer_key,
-	  consumer_secret      : process.env.consumer_secret,
-	  access_token_key     : process.env.access_token_key,
-	  access_token_secret  : process.env.access_token_secret
-	});
-	 
-	var params = {screen_name: process.env.screen_name,
-	count: 200};
-	client.get('statuses/home_timeline', params, function(error, tweets, response){
-	  if (!error) {
-	  	 sendit(tweets);  // now generate a mail from it 
-	  }
-	  if (error) {
-	  	console.log(error);	
-	  }
-	});
+     
+    var client = new Twitter({
+      consumer_key         : process.env.consumer_key,
+      consumer_secret      : process.env.consumer_secret,
+      access_token_key     : process.env.access_token_key,
+      access_token_secret  : process.env.access_token_secret
+    });
+     
+    var params = {screen_name: process.env.screen_name,
+    count: 200};
+    client.get('statuses/home_timeline', params, function(error, tweets, response){
+      if (!error) {
+         sendit(build_mail_content(tweets),tweets.length);  // now generate a mail from it 
+      }
+      if (error) {
+        console.log(error); 
+      }
+    });
 }
 
 //------------------------------- Send mail --------------------------
-
-
-function sendit (twitts)
+function build_mail_content (twitts)
 {
+
+ var  htmlbody ;
+
+    htmlbody ='<b> items ' +twitts.length +'</b>'
+    htmlbody+='<table style="width:100%" border="1">'
+
+    twitts.forEach(function additemtomail(t){
+        htmlbody+='<tr>'
+        htmlbody+='<td><img src="' +t.user.profile_image_url +'" style="width:32px;height:32px;"></td> '
+        htmlbody+='<td>'+ t.text ;
+    
+        
+        console.log(t);
+        mda=t.entities.media;
+        if (typeof(mda) != "undefined"){
+            console.log(">>>>>>>>");
+            mda.forEach(function prnt(tt){
+                    console.log(tt);
+                    console.log("-----"+tt.sizes.small.w);
+                    htmlbody+='<img src="' +tt.media_url +'" style="width:'+tt.sizes.small.w+'px;height:'+tt.sizes.small.h+'px;"> '
+            });
+            console.log("<<<<<");
+        }
+
+         htmlbody+='</td></tr>';
+
+    }) ; 
+    htmlbody+='</table>';
+
+     console.log(htmlbody);
+
+    return  htmlbody;
+}
+
+function sendit (html_body,item_count)
+{
+
+    console.log("******************"+ item_count);
     // create reusable transporter object using SMTP transport
     var transporter = nodemailer.createTransport({
         service: 'Gmail',
@@ -54,32 +90,18 @@ function sendit (twitts)
 
     var ts_hms = new Date;
 
-    var  htmlbody ;
-
-    htmlbody ='<b> items ' +twitts.length +'</b>'
-    htmlbody+='<table style="width:100%" border="1">'
-
-    twitts.forEach(function additemtomail(t){
-        htmlbody+='<tr>'
-        htmlbody+='<td><img src="' +t.user.profile_image_url +'" style="width:32px;height:32px;"></td> '
-        htmlbody+='<td>'+ t.text + '</td>';
-    
-        htmlbody+='</tr>';
-    }) ; 
-    htmlbody+='</table>';
-
-     console.log(htmlbody);
+   
     // setup e-mail data with unicode symbols
     var mailOptions = {
-        from: 'twittermaildigest@gmail.com ✔ <twittermaildigest@gmail.com>', // sender address
+        from: 'twittermaildigest@nomail.com ✔ <twittermaildigest@nomail.com>', // sender address
         to: process.env.send_digest_to_email, // list of receivers
         subject: 'Twitter Mail Digest ✔'+ts_hms.toISOString(), // Subject line
-        text: 'items' +twitts.length, // plaintext body
-        html: htmlbody
+        text: 'items' +item_count, // plaintext body
+        html: html_body
     };
 
 
-
+console.log(mailOptions);
 
     // send mail with defined transport object
     transporter.sendMail(mailOptions, function(error, info){
@@ -87,6 +109,7 @@ function sendit (twitts)
             return console.log(error);
         }
         console.log('Message sent: ' + info.response);
+        console.log("------------wait 6 Hours");
 
     });
 }
@@ -94,11 +117,10 @@ function sendit (twitts)
 
 //---------------------------------------  run ----------------------
 
-main();  			// run once 
+main();             // run once 
 
 setInterval(function(){
   main();
-}, 1000*60*60*24 );  // now  run every 24 hours 
+}, 1000*60*60*6 );  // now  run every 24 hours 
 
 
-console.log("next call is in next 24 Hours ");
