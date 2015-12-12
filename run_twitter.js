ea38365cbbcc2edc1bbd5a49c435e4241aa44d4c
neatsun.ziv <neatsunz@gmail.com>
2015-12-12 17:30:39 +0200

    itterate on all msgs - still not 100%

--- run_twitter.js
+++ run_twitter.js
@@ -1,39 +1,80 @@
+//----------------------------------------------------------------------------------------------------------------------------------\\
+//  ______     __  __     ______     ______     ______        ______   __     __     __     ______   ______   ______     ______     \\
+// /\  ___\   /\ \_\ \   /\  == \   /\  ___\   /\  == \      /\__  _\ /\ \  _ \ \   /\ \   /\__  _\ /\__  _\ /\  ___\   /\  == \    \\
+// \ \ \____  \ \____ \  \ \  __<   \ \  __\   \ \  __<      \/_/\ \/ \ \ \/ ".\ \  \ \ \  \/_/\ \/ \/_/\ \/ \ \  __\   \ \  __<    \\
+//  \ \_____\  \/\_____\  \ \_____\  \ \_____\  \ \_\ \_\       \ \_\  \ \__/".~\_\  \ \_\    \ \_\    \ \_\  \ \_____\  \ \_\ \_\  \\
+//   \/_____/   \/_____/   \/_____/   \/_____/   \/_/ /_/        \/_/   \/_/   \/_/   \/_/     \/_/     \/_/   \/_____/   \/_/ /_/  \\
+//----------------------------------------------------------------------------------------------------------------------------------\\
+                                                                                                                               
+
+
 var nodemailer = require('nodemailer');
- 
-var Twitter = require('twitter');
+var Twitter    = require('twitter');
 
-console.log('consumer_key= '        + process.env.consumer_key);
-console.log('consumer_secret= '     + process.env.consumer_secret);
-console.log('access_token_key= '    + process.env.access_token_key);
-console.log('access_token_secret= ' + process.env.access_token_secret);
-console.log('screen_name= '         + process.env.screen_name);
+/* // for Debug of Env.:
+    console.log('consumer_key= '        + process.env.consumer_key);
+    console.log('consumer_secret= '     + process.env.consumer_secret);
+    console.log('access_token_key= '    + process.env.access_token_key);
+    console.log('access_token_secret= ' + process.env.access_token_secret);
+    console.log('screen_name= '         + process.env.screen_name);
 
-console.log('gmail_sender_email= '  + process.env.gmail_sender_email);
-console.log('gmail_sender_pass= '   + process.env.gmail_sender_pass);
-console.log('send_digest_to_email= '+ process.env.send_digest_to_email);
+    console.log('gmail_sender_email= '  + process.env.gmail_sender_email);
+    console.log('gmail_sender_pass= '   + process.env.gmail_sender_pass);
+    console.log('send_digest_to_email= '+ process.env.send_digest_to_email);
+*/
 
-
+//---------------------------------  Main : init params and call get twitts ------------------------
 function main ()
 {
-     
     var client = new Twitter({
       consumer_key         : process.env.consumer_key,
       consumer_secret      : process.env.consumer_secret,
       access_token_key     : process.env.access_token_key,
       access_token_secret  : process.env.access_token_secret
     });
-     
-    var params = {screen_name: process.env.screen_name,
-    count: 200};
-    client.get('statuses/home_timeline', params, function(error, tweets, response){
-      if (!error) {
-         sendit(build_mail_content(tweets),tweets.length);  // now generate a mail from it 
-      }
-      if (error) {
-        console.log(error); 
-      }
-    });
+
+    var params=  {screen_name: process.env.screen_name,  count: 200};
+    var fulltweets="";
+
+  //------------------------------------ Get twitter data ---------------------------------
+    function get_twitts()
+    {
+        console.log ("---get twitter with :" +JSON.stringify(params));
+
+        client.get('statuses/home_timeline', params, function(error, tweets, response){
+            if (!error) {           //-------------- got first 200 tweets 
+
+                var last_tweet=tweets[tweets.length-1];
+                fulltweets+=JSON.stringify(tweets);
+                            //console.log(fulltweets.length);
+                        
+                var difff= Date.now() - Date.parse(last_tweet.created_at);
+                            // console.log("diff " +difff + " vs " + 1000*60*60*24);  // debug itteration till we get 24 H
+                if (difff <1000*60*60*24)  // check we have all that was tweeted in the past 24 Hours 
+                 {
+                    console.log("we are not done- now get more twitts ");
+                    params = {screen_name: process.env.screen_name,  count: 200, max_id:last_tweet.id };  //get from last tweet we got 
+                    get_twitts();
+                 }
+                else
+                {
+                    console.log("we are done!!! -> sending to mail size:"+fulltweets.length);
+                    var t= JSON.parse(fulltweets);
+                    sendit(build_mail_content(t),t.length);  // now generate a mail from it 
+                }
+            }
+            if (error) { // probably couldnt login or over quota 
+                console.log("Error getting twitter home : " + JSON.stringify(error)); 
+            }
+
+        });
+    }
+
+    get_twitts(); // run for the first time 
 }
+
+
+
 
 //------------------------------- Send mail --------------------------
 function build_mail_content (twitts)
